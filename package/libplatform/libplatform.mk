LuckInzz/labsisop-buildroot

commit 434357301311dcccd7d689d95bc9da706736a3f3

package/libplatform: bump version
Add -std=c++11 to CXXFLAGS to fix build with gcc < 5.x:

buildroot/output/build/libplatform-a7cd0d5780ed80a4e70480d1650749f29e8a1fb2/src/util/StringUtils.cpp:
 In static member function 'static std::string& StringUtils::TrimLeft(std::string&)':
buildroot/output/build/libplatform-a7cd0d5780ed80a4e70480d1650749f29e8a1fb2/src/util/StringUtils.cpp:456:99:
 warning: lambda expressions only available with -std=c++11 or -std=gnu++11 [enabled by default]
   str.erase(str.begin(), ::find_if(str.begin(), str.end(), [](char s) { return isspace_c(s) == 0; }));
                                                                                                   ^
buildroot/output/build/libplatform-a7cd0d5780ed80a4e70480d1650749f29e8a1fb2/src/util/StringUtils.cpp:456:100:
 error: no matching function for call to 'find_if(std::basic_string<char>::iterator, std::basic_string<char>::iterator,
  StringUtils::TrimLeft(std::string&)::__lambda0)'
   str.erase(str.begin(), ::find_if(str.begin(), str.end(), [](char s) { return isspace_c(s) == 0; }));
                                                                                                    ^

Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/libplatform/libplatform.mk
@@ -4,10 +4,14 @@
 #
 ################################################################################
 
-LIBPLATFORM_VERSION = 1c9d14fa996af33760a2c700caebd2bd9ae527c9
+LIBPLATFORM_VERSION = a7cd0d5780ed80a4e70480d1650749f29e8a1fb2
 LIBPLATFORM_SITE = $(call github,Pulse-Eight,platform,$(LIBPLATFORM_VERSION))
 LIBPLATFORM_LICENSE = GPL-2.0+
 LIBPLATFORM_LICENSE_FILES = src/os.h
 LIBPLATFORM_INSTALL_STAGING = YES
+
+ifeq ($(BR2_TOOLCHAIN_GCC_AT_LEAST_5),)
+LIBPLATFORM_CONF_OPTS += -DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -std=c++11"
+endif
 
 $(eval $(cmake-package))

package/libplatform/libplatform.hash
@@ -1,3 +1,3 @@
 # Locally computed
-sha256  209daeb87af4ca92199887eb151b2a459bb81566cdaa092644e0889afb64bf6a  libplatform-1c9d14fa996af33760a2c700caebd2bd9ae527c9.tar.gz
+sha256  ebee46c76b86b40b703fa913465f5004fa3166eeb6a4794c58fc8a5c25702931  libplatform-a7cd0d5780ed80a4e70480d1650749f29e8a1fb2.tar.gz
 sha256  80fc66febbb2393c9d518e3eefa4ad9aae15a9b6a84174c0e443ea79eeea5131  src/os.h

commit af06436ef8b6a48c5b6460eee6bdd808e39735f5

package/libplatform: fix build with gcc 5
Build with gcc 5 is broken since latest bump in commit
434357301311dcccd7d689d95bc9da706736a3f3

Instead of updating workaround, use a patch that has been submitted
upstream

Fixes:
 - http://autobuild.buildroot.org/results/2b1922f99b1c213b4e28a5b1a11879f4e28c202f

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/libplatform/0001-cmake-require-c-11-as-the-minimum-standard.patch
@@ -1,0 +1,26 @@
+From ad9fcceb6267e737316b616551b7aac3cf676481 Mon Sep 17 00:00:00 2001
+From: Lukas Rusak <lorusak@gmail.com>
+Date: Fri, 2 Oct 2020 15:31:36 -0700
+Subject: [PATCH] [cmake] require c++11 as the minimum standard
+
+Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
+[Upstream status: https://github.com/Pulse-Eight/platform/pull/46]
+---
+ CMakeLists.txt | 4 ++++
+ 1 file changed, 4 insertions(+)
+
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 7144b0b..e9112f9 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -3,6 +3,10 @@ project(p8-platform)
+ cmake_minimum_required(VERSION 2.8.9)
+ enable_language(CXX)
+ 
++set(CMAKE_CXX_STANDARD 11)
++set(CMAKE_CXX_STANDARD_REQUIRED YES)
++set(CMAKE_CXX_EXTENSIONS NO)
++
+ set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${PROJECT_SOURCE_DIR})
+ 
+ find_package(Threads REQUIRED)

package/libplatform/libplatform.mk
@@ -10,8 +10,4 @@ LIBPLATFORM_LICENSE = GPL-2.0+
 LIBPLATFORM_LICENSE_FILES = src/os.h
 LIBPLATFORM_INSTALL_STAGING = YES
 
-ifeq ($(BR2_TOOLCHAIN_GCC_AT_LEAST_5),)
-LIBPLATFORM_CONF_OPTS += -DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -std=c++11"
-endif
-
 $(eval $(cmake-package))