LuckInzz/labsisop-buildroot

commit cc32e3388fb6eab99c6e60091f48de52d73d6f75

package/wireplumber: new package
Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

package/wireplumber/wireplumber.mk
@@ -1,0 +1,39 @@
+################################################################################
+#
+# wireplumber
+#
+################################################################################
+
+WIREPLUMBER_VERSION = 0.4.2
+WIREPLUMBER_SOURCE = wireplumber-$(WIREPLUMBER_VERSION).tar.bz2
+WIREPLUMBER_SITE = https://gitlab.freedesktop.org/pipewire/wireplumber/-/archive/$(WIREPLUMBER_VERSION)
+WIREPLUMBER_LICENSE = MIT
+WIREPLUMBER_LICENSE_FILES = LICENSE
+WIREPLUMBER_DEPENDENCIES = host-pkgconf dbus pipewire libglib2 lua
+
+WIREPLUMBER_CONF_OPTS = \
+	-Ddoc=disabled \
+	-Dsystem-lua=true \
+	-Dsystem-lua-version=
+
+ifeq ($(BR2_PACKAGE_GOBJECT_INTROSPECTION),y)
+WIREPLUMBER_DEPENDENCIES += gobject-introspection
+WIREPLUMBER_CONF_OPTS += -Dintrospection=enabled
+else
+WIREPLUMBER_CONF_OPTS += -Dintrospection=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_SYSTEMD),y)
+WIREPLUMBER_DEPENDENCIES += systemd
+WIREPLUMBER_CONF_OPTS += \
+	-Dsystemd=enabled \
+	-Dsystemd-system-service=true \
+	-Dsystemd-user-service=true
+else
+WIREPLUMBER_CONF_OPTS += \
+	-Dsystemd=disabled \
+	-Dsystemd-system-service=false \
+	-Dsystemd-user-service=false
+endif
+
+$(eval $(meson-package))

package/Config.in
@@ -1539,6 +1539,7 @@ menu "Graphics"
 	source "package/waylandpp/Config.in"
 	source "package/webkitgtk/Config.in"
 	source "package/webp/Config.in"
+	source "package/wireplumber/Config.in"
 	source "package/wlroots/Config.in"
 	source "package/woff2/Config.in"
 	source "package/wpebackend-fdo/Config.in"

package/wireplumber/Config.in
@@ -1,0 +1,22 @@
+config BR2_PACKAGE_WIREPLUMBER
+	bool "wireplumber"
+	depends on BR2_PACKAGE_PIPEWIRE
+	depends on BR2_PACKAGE_LUA_5_3 || BR2_PACKAGE_LUA_5_4
+	depends on BR2_USE_WCHAR # libglib2
+	depends on BR2_TOOLCHAIN_HAS_THREADS # libglib2
+	depends on BR2_USE_MMU # libglib2
+	select BR2_PACKAGE_LIBGLIB2
+	help
+	  WirePlumber is a modular session / policy manager for
+	  PipeWire and a GObject-based high-level library that
+	  wraps PipeWire’s API, providing convenience for writing
+	  the daemon’s modules as well as external tools for
+	  managing PipeWire.
+
+	  https://pipewire.pages.freedesktop.org/wireplumber/
+
+comment "wireplumber needs a toolchain w/ wchar, threads and Lua >= 5.3"
+	depends on BR2_PACKAGE_PIPEWIRE
+	depends on BR2_USE_MMU
+	depends on !BR2_USE_WCHAR || !BR2_TOOLCHAIN_HAS_THREADS || \
+		(!BR2_PACKAGE_LUA_5_3 && !BR2_PACKAGE_LUA_5_4)

package/wireplumber/wireplumber.hash
@@ -1,0 +1,3 @@
+# Locally calculated
+sha256  41f174eaae5a630e195001fdd580918fa5143a71edceb050b320f85a8e65e53b  wireplumber-0.4.2.tar.bz2
+sha256  ff2eb9af5b05d92ac3a38cffcad27582ef555a31ff1781e1c1d4e087f77c9c81  LICENSE

commit 36fbd5dbb2185b1bc1e7099d29658d81cbf8c630

package/wireplumber: fix package by making the dbus dependency optional
The wireplumber currently as "dbus" in its DEPENDENCIES, but it does
not select/depend on it at the Kconfig level. A simple configuration
such as:

BR2_PACKAGE_LUA=y
BR2_PACKAGE_PIPEWIRE=y
BR2_PACKAGE_WIREPLUMBER=y

Therefore fails to build with:

Makefile:576: *** dbus is in the dependency chain of wireplumber that has added it to its _DEPENDENCIES variable without selecting it or depending on it from Config.in.  Stop.

Since dbus is actually an optional dependency of wireplumber, this
commit fixes the issue by really handling it as an optional
dependency.

Fixes: cc32e3388fb6eab99c6e60091f48de52d73d6f75 ("package/wireplumber: new package")
Signed-off-by: Théo Lebrun <theo.lebrun@bootlin.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/wireplumber/wireplumber.mk
@@ -9,12 +9,16 @@ WIREPLUMBER_SOURCE = wireplumber-$(WIREPLUMBER_VERSION).tar.bz2
 WIREPLUMBER_SITE = https://gitlab.freedesktop.org/pipewire/wireplumber/-/archive/$(WIREPLUMBER_VERSION)
 WIREPLUMBER_LICENSE = MIT
 WIREPLUMBER_LICENSE_FILES = LICENSE
-WIREPLUMBER_DEPENDENCIES = host-pkgconf dbus pipewire libglib2 lua
+WIREPLUMBER_DEPENDENCIES = host-pkgconf pipewire libglib2 lua
 
 WIREPLUMBER_CONF_OPTS = \
 	-Ddoc=disabled \
 	-Dsystem-lua=true \
 	-Dsystem-lua-version=
+
+ifeq ($(BR2_PACKAGE_DBUS),y)
+WIREPLUMBER_DEPENDENCIES += dbus
+endif
 
 ifeq ($(BR2_PACKAGE_GOBJECT_INTROSPECTION),y)
 WIREPLUMBER_DEPENDENCIES += host-doxygen host-python-lxml gobject-introspection