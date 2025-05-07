ffabd6ade48f83fc68b4fea21ac22e184bd7b4e8
Lucca Ianez <111288789+LuckInzz@users.noreply.github.com>
2025-05-07 22:07:59 +0000

    Tutorial 2.3 finalizado.

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
-
 cp $BASE_DIR/../custom-scripts/linuxstatus.py $BASE_DIR/target/usr/bin/ 
 chmod +x $BASE_DIR/target/usr/bin/linuxstatus.py 
-
 cp $BASE_DIR/../custom-scripts/S99linuxstatus $BASE_DIR/target/etc/init.d/ 
 chmod +x $BASE_DIR/target/etc/init.d/S99linuxstatus
+make -C $BASE_DIR/../modules/simple_driver/
